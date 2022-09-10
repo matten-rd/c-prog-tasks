e9cbc9182b94a7ccc47d94b9dfa703fb01d6e9d4
matten-rd <strandfilip21@gmail.com>
2022-09-10 14:34:35 +0100

    Working first version of the full program

--- task1/main.c
+++ task1/main.c
@@ -7,21 +7,54 @@ typedef struct TemperatureStore {
     unsigned maxTemp;
     unsigned minTemp;
     float avgTemp;
-    char *tempLog[10];
+    int currentLogIndex;
+    unsigned tempLog[10];
 } TemperatureStore;
 
 int main(int argc, char const *argv[])
 {
     char action[3];
 
+    TemperatureStore tempStore = {
+        .currentLogIndex = 0,
+        .avgTemp = 0.0
+    };
+
     while (1)
     {
         scanf("%s", action);
-        if (strlen(action) > 1)
+        int actionLenght = strlen(action);
+        if (actionLenght > 1)
         {
-            if (strlen(action) == 3 && action[0] == 'T' && isdigit((unsigned char) action[1]) && isdigit((unsigned char) action[2]))
+            if (actionLenght == 3 && action[0] == 'T' && isdigit((unsigned char)action[1]) && isdigit((unsigned char)action[2]))
             {
-                printf("Received Temperature: %c%c\n", action[1], action[2]);
+                if (tempStore.currentLogIndex < 3)
+                {
+                    // Extract recieved temperature
+                    char subString[3];
+                    memcpy(subString, &action[1], 2);
+                    subString[2] = '\0';
+                    unsigned recievedTemp = atoi(subString);
+
+                    // Set min and max temp
+                    if (tempStore.maxTemp < recievedTemp || tempStore.currentLogIndex == 0) tempStore.maxTemp = recievedTemp;
+                    if (tempStore.minTemp > recievedTemp || tempStore.currentLogIndex == 0) tempStore.minTemp = recievedTemp;
+
+                    // Calculate avg temp
+                    tempStore.avgTemp = tempStore.avgTemp + ((recievedTemp - tempStore.avgTemp) / (tempStore.currentLogIndex+1));
+
+                    // Add recieved temp to log array
+                    tempStore.tempLog[tempStore.currentLogIndex] = recievedTemp;
+
+                    // Increment "pointer" to current index
+                    tempStore.currentLogIndex++;
+
+                    // Print temperature
+                    printf("Received Temperature: %d\n", recievedTemp);
+                } else
+                {
+                    printf("Log Full\n");
+                }
             } else
             {
                 printf("Input Error Length\n");
@@ -31,22 +64,34 @@ int main(int argc, char const *argv[])
             switch (action[0])
             {
                 case 'A':
-                    printf("Average Temperature: 19.90\n");
+                    if (tempStore.currentLogIndex > 0) {
+                        printf("Average Temperature: %.2f\n", tempStore.avgTemp);
+                    } else {
+                        printf("Average Temperature: N/A\n");
+                    }
                     break;
 
                 case 'N':
-                    printf("Minimum Temperature: 15\n");
+                    if (tempStore.currentLogIndex > 0) {
+                        printf("Minimum Temperature: %d\n", tempStore.minTemp);
+                    } else {
+                        printf("Minimum Temperature: N/A\n");
+                    }
                     break;
 
                 case 'X':
-                    printf("Maximum Temperature: 23\n");
+                    if (tempStore.currentLogIndex > 0) {
+                        printf("Maximum Temperature: %d\n", tempStore.maxTemp);
+                    } else {
+                        printf("Maximum Temperature: N/A\n");
+                    }
                     break;
 
                 case 'L':
-                    printf("Log: 10 entries\n");
-                    for (int i = 0; i < 10; i++)
+                    printf("Log: %d entries\n", tempStore.currentLogIndex);
+                    for (int i = 0; i < tempStore.currentLogIndex; i++)
                     {
-                        printf("Temperature: %d\n", i);
+                        printf("Temperature: %d\n", tempStore.tempLog[i]);
                     }
                     break;
 
