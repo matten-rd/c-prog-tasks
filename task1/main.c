dd173a16daf77dfb8d1850141d16dd0b747ba5b1
matten-rd <strandfilip21@gmail.com>
2022-09-10 20:01:51 +0100

    Cleaned up the code and fixed some bugs that Kattis does not check for

--- task1/main.c
+++ task1/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 
+// Struct to store the log data in
 typedef struct TemperatureStore {
     unsigned maxTemp;
     unsigned minTemp;
@@ -11,122 +12,144 @@ typedef struct TemperatureStore {
     unsigned tempLog[10];
 } TemperatureStore;
 
-int is_valid_temperature(char *ch)
+int is_valid_number(char *ch)
 {
+    // To catch when input is just 'T'
     int length = strlen(ch);
     if (length < 1) return 0;
     
+    // Checks if any of the chars are NOT digits and returns 0 in that case
     for (int i = 0; i < length; i++)
     {
-        if (!isdigit((unsigned char)ch[i]))
-        {
-            return 0;
-        }
+        if (!isdigit((unsigned char)ch[i])) return 0;
     }
+    // Return 1 if all chars are digits
     return 1;
+}
+
+void handle_user_request(char action, TemperatureStore *tempStore)
+{
+    // Handles all possible user actions
+    switch (action)
+    {
+        case 'A':
+            if (tempStore->currentLogIndex > 0)
+            {
+                printf("Average Temperature: %.2f\n", tempStore->avgTemp);
+            }
+            else
+            {
+                printf("Average Temperature: N/A\n");
+            }
+            break;
+
+        case 'N':
+            if (tempStore->currentLogIndex > 0)
+            {
+                printf("Minimum Temperature: %d\n", tempStore->minTemp);
+            }
+            else
+            {
+                printf("Minimum Temperature: N/A\n");
+            }
+            break;
+
+        case 'X':
+            if (tempStore->currentLogIndex > 0)
+            {
+                printf("Maximum Temperature: %d\n", tempStore->maxTemp);
+            }
+            else
+            {
+                printf("Maximum Temperature: N/A\n");
+            }
+            break;
+
+        case 'L':
+            printf("Log: %d entries\n", tempStore->currentLogIndex);
+            for (int i = 0; i < tempStore->currentLogIndex; i++)
+            {
+                printf("Temperature: %d\n", tempStore->tempLog[i]);
+            }
+            break;
+
+        case 'Q':
+            printf("Exiting...\n");
+            exit(0);
+            break;
+
+        default:
+            printf("Input Error\n");
+            break;
+    }
+}
+
+void handle_temp_log(char *temperature, TemperatureStore *tempStore) 
+{
+    // Convert to int (checked previously that this is safe)
+    unsigned receivedTemp = atoi(temperature);
+    // Set max temp
+    if (tempStore->maxTemp < receivedTemp || tempStore->currentLogIndex == 0)
+        tempStore->maxTemp = receivedTemp;
+    // Set min temp
+    if (tempStore->minTemp > receivedTemp || tempStore->currentLogIndex == 0)
+        tempStore->minTemp = receivedTemp;
+
+    // Calculate avg temp
+    tempStore->avgTemp = tempStore->avgTemp + ((receivedTemp - tempStore->avgTemp) / (tempStore->currentLogIndex + 1));
+
+    // Add received temp to log array
+    tempStore->tempLog[tempStore->currentLogIndex] = receivedTemp;
+
+    // Increment currentLogIndex
+    tempStore->currentLogIndex++;
+
+    // Print temperature
+    printf("Received Temperature: %d\n", receivedTemp);
 }
 
 int main(int argc, char const *argv[])
 {
-    char test[32];
-    char *action = test;
+    // Initialise a pointer for the log input
+    char arr[10];
+    char *logInput = arr;
+
     int sc;
+
     TemperatureStore tempStore = {
-        .maxTemp = 0,
-        .minTemp = 0,
         .avgTemp = 0.0,
-        .currentLogIndex = 0,
-        .tempLog = {0}
+        .currentLogIndex = 0
     };
 
     while (1)
     {
-        sc = scanf("%s", action);
+        // read log input
+        sc = scanf("%s", logInput);
         if (sc != 1) printf("Epic fail");
-        
-        switch (action[0])
+
+        if (strlen(logInput) == 1) // User commands are always 1 char
         {
-            case 'T':
-                if (tempStore.currentLogIndex < 10)
-                {
-                    // Increment pointer past 'T' and check if the rest is a valid number
-                    action++; 
-                    if (!is_valid_temperature(action) && atoi(action) >= 0)
-                    {
-                        printf("Input Error\n");
-                    } 
-                    else 
-                    {
-                        unsigned recievedTemp = atoi(action);
-                        // Set max temp
-                        if (tempStore.maxTemp < recievedTemp || tempStore.currentLogIndex == 0)
-                            tempStore.maxTemp = recievedTemp;
-                        // Set min temp
-                        if (tempStore.minTemp > recievedTemp || tempStore.currentLogIndex == 0)
-                            tempStore.minTemp = recievedTemp;
-
-                        // Calculate avg temp
-                        tempStore.avgTemp = tempStore.avgTemp+((recievedTemp-tempStore.avgTemp) / (tempStore.currentLogIndex+1));
-
-                        // Add recieved temp to log array
-                        tempStore.tempLog[tempStore.currentLogIndex] = recievedTemp;
-
-                        // Increment currentLogIndex
-                        tempStore.currentLogIndex++;
-
-                        // Print temperature
-                        printf("Received Temperature: %d\n", recievedTemp);
-                    }
-                } 
-                else
-                {
-                    printf("Log Full\n");
-                }
-                break;
-
-            case 'A':
-                if (tempStore.currentLogIndex > 0) {
-                    printf("Average Temperature: %.2f\n", tempStore.avgTemp);
-                } else {
-                    printf("Average Temperature: N/A\n");
-                }
-                break;
-
-            case 'N':
-                if (tempStore.currentLogIndex > 0) {
-                    printf("Minimum Temperature: %d\n", tempStore.minTemp);
-                } else {
-                    printf("Minimum Temperature: N/A\n");
-                }
-                break;
-
-            case 'X':
-                if (tempStore.currentLogIndex > 0) {
-                    printf("Maximum Temperature: %d\n", tempStore.maxTemp);
-                } else {
-                    printf("Maximum Temperature: N/A\n");
-                }
-                break;
-
-            case 'L':
-                printf("Log: %d entries\n", tempStore.currentLogIndex);
-                for (int i = 0; i < tempStore.currentLogIndex; i++)
-                {
-                    printf("Temperature: %d\n", tempStore.tempLog[i]);
-                }
-                break;
-
-            case 'Q':
-                printf("Exiting...\n");
-                exit(0);
-                break;
-
-            default:
+            handle_user_request(logInput[0], &tempStore);
+        }
+        else if (logInput[0] == 'T')
+        {
+            // Increment pointer past 'T' and check if the rest is a valid number (T20 -> 20)
+            logInput++;
+            if (!is_valid_number(logInput) && atoi(logInput) >= 0)
+            {
                 printf("Input Error\n");
-                break;
+            } else if (tempStore.currentLogIndex < 10) // Check that log is not full
+            {
+                handle_temp_log(logInput, &tempStore);
+            }
+            else
+            {
+                printf("Log Full\n");
+            }
+        } else // All other input is not allowed
+        {
+            printf("Input Error\n");
         }
     }
-    
-
     return 0;
 }
