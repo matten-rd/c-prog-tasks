4bd27cccac1b6b7e705f2191f8c32865e9e1ad44
matten-rd <strandfilip21@gmail.com>
2022-09-14 14:07:29 +0100

    First draft of task 4

---
+++ task4/TemperatureAndHumidity.h
@@ -0,0 +1,35 @@
+#ifndef TEMPERATUREANDHUMIDITY_H
+#define TEMPERATUREANDHUMIDITY_H
+
+typedef struct TemperatureAndHumidityStore
+{
+    int logIndex;
+    int logSize;
+    int logIncrement;
+    unsigned maxTemperature;
+    unsigned minTemperature;
+    float avgTemperature;
+    unsigned *temperatureLog;
+    unsigned maxHumidity;
+    unsigned minHumidity;
+    float avgHumidity;
+    unsigned *humidityLog;
+} TemperatureAndHumidityStore;
+
+TemperatureAndHumidityStore TemperatureAndHumidity_init();
+
+int TemperatureAndHumidity_get_log_increment(int message);
+
+void TemperatureAndHumidity_set_log_increment(int newIncrement, TemperatureAndHumidityStore *self);
+
+void TemperatureAndHumidity_update_log_size(TemperatureAndHumidityStore *self);
+
+void TemperatureAndHumidity_update(int humidity, int temperature, TemperatureAndHumidityStore *self);
+
+int TemperatureAndHumidity_get_type(int message);
+
+int TemperatureAndHumidity_get_humidity(int message);
+
+int TemperatureAndHumidity_get_temperature(int message);
+
+#endif // TEMPERATUREANDHUMIDITY_H
---
+++ task4/main.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "TemperatureAndHumidity.h"
+
+int is_valid_hex(char *ch)
+{
+    // Handle case when input is empty
+    int length = strlen(ch);
+    if (length < 1)
+        return 0;
+
+    // Check that all chars are valid hex digits
+    if (ch[strspn(ch, "0123456789abcdefxABCDEFX")] == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void handle_valid_input(int type, int humidity, int temperature, TemperatureAndHumidityStore *thStore)
+{
+    switch (type)
+    {
+    case 0b000:
+        if (thStore->logIndex >= thStore->logSize)
+        {
+            TemperatureAndHumidity_update_log_size(thStore);
+            printf("Log Size Expanded To: %d\n", thStore->logSize);
+        }
+        TemperatureAndHumidity_update(humidity, temperature, thStore);
+        printf("Received Temperature: %d\n", temperature);
+        printf("Received Humidity: %d\n", humidity);
+        break;
+
+    case 0b010:
+        if (thStore->logIndex > 0)
+        {
+            printf("Average Temperature: %.2f\n", thStore->avgTemperature);
+            printf("Average Humidity: %.2f\n", thStore->avgHumidity);
+        }
+        else
+        {
+            printf("Average Temperature: N/A\n");
+            printf("Average Humidity: N/A\n");
+        }
+        break;
+
+    case 0b011:
+        if (thStore->logIndex > 0)
+        {
+            printf("Minimum Temperature: %d\n", thStore->minTemperature);
+            printf("Minimum Humidity: %d\n", thStore->minHumidity);
+        }
+        else
+        {
+            printf("Minimum Temperature: N/A\n");
+            printf("Minimum Humidity: N/A\n");
+        }
+        break;
+
+    case 0b100:
+        if (thStore->logIndex > 0)
+        {
+            printf("Maximum Temperature: %d\n", thStore->maxTemperature);
+            printf("Maximum Humidity: %d\n", thStore->maxHumidity);
+        }
+        else
+        {
+            printf("Maximum Temperature: N/A\n");
+            printf("Maximum Humidity: N/A\n");
+        }
+        break;
+
+    case 0b101:
+        printf("Log: %d entries\n", thStore->logIndex);
+        for (int i = 0; i < thStore->logIndex; i++)
+        {
+            printf("Temperature: %d; Humidity: %d\n", thStore->temperatureLog[i], thStore->humidityLog[i]);
+        }
+        break;
+
+    default:
+        printf("Input Error\n");
+        break;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    char logInput[32];
+
+    int message, type, humidity, temperature, newIncrement;
+
+    TemperatureAndHumidityStore temperatureAndHumidityStore = TemperatureAndHumidity_init();
+
+    while (1)
+    {
+        // read log input
+        if (!fgets(logInput, sizeof(logInput), stdin))
+        {
+            printf("Input Error\n");
+        }
+        // Remove trailing newline to make a proper string
+        logInput[strcspn(logInput, "\r\n")] = 0;
+
+        if (!is_valid_hex(&logInput))
+        {
+            printf("Input Error\n");
+        }
+        else
+        {
+            sscanf(logInput, "%x", &message);
+
+            // Here message is a valid hexnumber
+            type = TemperatureAndHumidity_get_type(message);
+
+            switch (type)
+            {
+            case 0b111:
+                newIncrement = TemperatureAndHumidity_get_log_increment(message);
+                TemperatureAndHumidity_set_log_increment(newIncrement, &temperatureAndHumidityStore);
+                printf("New Log Increment: %d\n", newIncrement);
+                printf("Current Log Size: %d\n", temperatureAndHumidityStore.logSize);
+                break;
+
+            case 0b110:
+                printf("Exiting...\n");
+                exit(0);
+                break;
+
+            default:
+                if (temperatureAndHumidityStore.logSize == 0)
+                {
+                    printf("Log Not Initialized\n");
+                }
+                else 
+                {
+                    humidity = TemperatureAndHumidity_get_humidity(message);
+                    temperature = TemperatureAndHumidity_get_temperature(message);
+                    handle_valid_input(type, humidity, temperature, &temperatureAndHumidityStore);
+                }
+                break;
+            }
+            
+        }
+    }
+    return 0;
+}
