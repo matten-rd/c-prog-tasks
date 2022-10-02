09d4c96e5e65b68a81b6fd9caf3864187cdbcb6b
matten-rd <strandfilip21@gmail.com>
2022-10-02 11:25:24 +0100

    Added doc comments to header file

--- task3/TemperatureAndHumidity.h
+++ task3/TemperatureAndHumidity.h
@@ -1,6 +1,9 @@
 #ifndef TEMPERATUREANDHUMIDITY_H
 #define TEMPERATUREANDHUMIDITY_H
 
+/**
+ * Struct to store all raw log data and aggregate data to be queried by the user
+ */
 typedef struct TemperatureAndHumidityStore
 {
     int currentLogIndex;
@@ -14,14 +17,31 @@ typedef struct TemperatureAndHumidityStore
     unsigned humidityLog[10];
 } TemperatureAndHumidityStore;
 
+/**
+ * Initialise a clean TemperatureAndHumidityStore struct
+ *
+ * @return TemperatureAndHumidityStore
+ */
 TemperatureAndHumidityStore TemperatureAndHumidity_init();
 
+/**
+ * Update the TemperatureAndHumidityStore struct when receiving a temperature and humidity
+ */
 void TemperatureAndHumidity_update(int humidity, int temperature, TemperatureAndHumidityStore *self);
 
+/**
+ * Extract and return the type from a received message
+ */
 int TemperatureAndHumidity_get_type(int message);
 
+/**
+ * Extract and return the humidity from a received message
+ */
 int TemperatureAndHumidity_get_humidity(int message);
 
+/**
+ * Extract and return the temperature from a received message
+ */
 int TemperatureAndHumidity_get_temperature(int message);
 
 #endif // TEMPERATUREANDHUMIDITY_H
